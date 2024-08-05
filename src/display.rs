851d0519efca08f5d14d4a6a94851120b402e3f4
Felix Salcher <felix.salcher@additive.eu>
2024-08-05 12:24:36 +0200

    fixed display of cursor

--- src/display.rs
+++ src/display.rs
@@ -28,18 +28,16 @@ impl Widget for Display<'_> {
         for (idx, row) in content.iter().enumerate() {
             let mut spans = vec![];
             if idx == cursor.row {
-                if row.is_empty() {
-                    spans.push(Span::styled(" ", Style::default().bg(Color::Black)))
-                } else if cursor.col == 0 {
-                    spans.push(Span::styled(&row[0..1], Style::default().bg(Color::Black)));
-                    spans.push(Span::raw(&row[cursor.col + 1..row.len()]));
+                if cursor.col == row.len() {
+                    spans.push(Span::raw(&row[0..cursor.col]));
+                    spans.push(Span::styled(" ", Style::default().bg(Color::Black)));
                 } else {
-                    spans.push(Span::raw(&row[0..cursor.col - 1]));
+                    spans.push(Span::raw(&row[0..cursor.col]));
                     spans.push(Span::styled(
-                        &row[cursor.col - 1..cursor.col],
+                        &row[cursor.col..cursor.col + 1],
                         Style::default().bg(Color::Black),
                     ));
-                    spans.push(Span::raw(&row[cursor.col..row.len()]));
+                    spans.push(Span::raw(&row[cursor.col + 1..row.len()]));
                 }
             } else {
                 spans.push(Span::raw(row));
