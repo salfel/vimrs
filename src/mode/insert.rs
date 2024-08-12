dbe069dd08a892375fc714280654699a8f7ac1ff
Felix Salcher <dev.felix.salcher@gmail.com>
2024-08-12 07:57:26 +0200

    implemented new_line

--- src/mode/insert.rs
+++ src/mode/insert.rs
@@ -6,6 +6,7 @@ pub fn handle_insert_keys(cx: &mut Context, event: KeyEvent) {
     match event.code {
         KeyCode::Char(key) => insert_char(cx, key),
         KeyCode::Backspace => pop_char(cx),
+        KeyCode::Enter => new_line(cx),
         KeyCode::Esc => {
             cx.change_mode(Mode::Normal);
             cx.cursor.col -= 1;
@@ -23,12 +24,32 @@ fn insert_char(cx: &mut Context, key: char) {
 
 fn pop_char(cx: &mut Context) {
     if let Some(line) = cx.content.get_mut(cx.cursor.row) {
-        // TODO implement remove line
         if cx.cursor.col == 0 {
+            if cx.cursor.row != 0 {
+                let content = cx.content.remove(cx.cursor.row);
+                let prev_row = cx
+                    .content
+                    .get_mut(cx.cursor.row - 1)
+                    .expect("row doesn't exist");
+
+                cx.cursor.row -= 1;
+                cx.cursor.col = prev_row.len();
+                prev_row.push_str(&content);
+            };
+
             return;
         }
 
         line.remove(cx.cursor.col - 1);
         cx.cursor.col -= 1;
+    }
+}
+
+fn new_line(cx: &mut Context) {
+    if let Some(line) = cx.content.get_mut(cx.cursor.row) {
+        let content = line.drain(cx.cursor.col..).collect();
+        cx.content.insert(cx.cursor.row + 1, content);
+        cx.cursor.row += 1;
+        cx.cursor.col = 0;
     }
 }
